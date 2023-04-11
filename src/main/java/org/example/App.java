a6c209b5eac483755efd00c4f05ade6234191c91
ChoboZilla <cheba7001@gmail.com>
2023-04-11 20:50:36 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import com.epam.tat.module4.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
